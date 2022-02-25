af86f1a6ae0006a9a6de6aa569a31dc2ac13bc52
Austin  Marrazza <austinmarrazza@Austins-MacBook-Pro.local>
2022-02-25 12:16:52 -0800

    reigster and login endpoints working and generating jwt

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
+const { checkUsernameFree, checkCredentials, checkUsernameExists } = require('../middleware/auth-middleware')
+const { JWT_SECRET } = require('./secrets');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../users/users-model');
 
-router.post('/register', (req, res) => {
-  res.end('implement register, please!');
+
+router.post('/register', checkCredentials, checkUsernameFree, (req, res, next) => {
+
+  const { username, password } = req.body;
+  const hash = bcrypt.hashSync(password, 8);
+  User.add({ username, password: hash })
+    .then(newUser => {
+      res.status(201).json(newUser)
+    }).catch(next)
+
+
+
+
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
@@ -29,8 +45,18 @@ router.post('/register', (req, res) => {
   */
 });
 
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
+router.post('/login', checkCredentials, checkUsernameExists, (req, res, next) => {
+  
+  if (bcrypt.compareSync(req.body.password, req.user.password)){
+    const token = buildToken(req.user)
+    res.json({
+      message: `welcome, ${req.user.username}`,
+      token
+    });
+  } else {
+    next({ status: 401, message: 'invalid credentials'})
+  }
+
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
@@ -54,6 +80,20 @@ router.post('/login', (req, res) => {
     4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
       the response body should include a string exactly as follows: "invalid credentials".
   */
+
 });
+
+
+
+function buildToken(user){
+  const payload = {
+    subject: user.id,
+    username: user.username
+  }
+  const options = {
+    expiresIn: '1d',
+  }
+  return jwt.sign(payload, JWT_SECRET, options)
+}
 
 module.exports = router;
---
+++ api/middleware/auth-middleware.js
@@ -0,0 +1,44 @@
+const User = require('../users/users-model')
+
+async function checkUsernameFree(req, res, next){
+    try{
+        const users = await User.findBy({ username: req.body.username })
+        if (!users.length){
+            next()
+        }
+        else {
+            next({ message: 'username taken', status: 422 })
+        }
+    } catch(err) {
+        next(err)
+    }
+}
+
+ function checkCredentials(req, res, next){
+    const { username, password } = req.body
+    if (!username || !password ){
+        next({ message: 'username and password required', status: 422 })
+    } else {
+        next()
+    }
+}
+
+async function checkUsernameExists(req, res, next){
+    try {
+        const users = await User.findBy({ username: req.body.username })
+        if (users.length){
+            req.user = users[0]
+            next()
+        } else{
+            next({ "message": "invalid credentials", status: 401 })
+        }
+    } catch (err) {
+        next(err)
+    }
+}
+
+module.exports = {
+    checkUsernameFree,
+    checkCredentials,
+    checkUsernameExists
+}
