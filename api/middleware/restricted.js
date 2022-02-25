25c03300400bab80a48ddf70a115175970f1c630
Austin  Marrazza <austinmarrazza@Austins-MacBook-Pro.local>
2022-02-25 14:13:50 -0800

    /api/jokes now restricted

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,20 @@
-module.exports = (req, res, next) => {
-  next();
+const { JWT_SECRET } = require('../auth/secrets')
+const jwt = require('jsonwebtoken')
+
+module.exports = async(req, res, next) => {
+  const token = req.headers.authorization
+
+  if(!token){
+    return next({ status: 401, message: 'token required'})
+  }
+
+  jwt.verify(token, JWT_SECRET, (err) => {
+    if(err){
+      next({ status: 401, message: 'token invalid'})
+    } else {
+      next()
+    }
+  })
   /*
     IMPLEMENT
 
